cca7b92d4c8e8b52b7dd8ad08480ba43eb7f399b
Juan José Mayorquín <juanjmayor@outlook.es>
2023-09-11 15:32:30 -0500

    Se añadieron animaciones al Hero.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,12 +12,12 @@ export function Hero() {
                 <span className="animate-breathe-7 animate-delay-0">A</span>
                 <span className="animate-breathe-9 animate-delay-200">S</span>
             </h1>
-            <p className="tracking-widest text-lg md:text-2xl lg:text-4xl animate-fade animate-delay-[1200ms] animate-duration-1000">
+            <p className="tracking-widest text-lg md:text-2xl lg:text-4xl animate-fade-down animate-delay-[1200ms] animate-duration-1000">
                 <span className="font-medium">50años</span>
                 <span className="font-extralight">entre</span>
                 <span className="font-bold">historias</span>
             </p>
-            <div className="absolute bottom-5 hover:text-lg hover:font-medium transition-all animate-fade animate-delay-[1700ms] animate-duration-1000">
+            <div className="absolute bottom-5 hover:text-lg hover:font-medium transition-all animate-fade-down animate-delay-[1700ms]">
                 <a className="flex flex-col items-center" href="">
                     <svg xmlns="http://www.w3.org/2000/svg" className="animate-pulse animate-duration-1000 w-14 h-14 hover:w-16 hover:h-16 hover:animate-none transition-all " viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
                       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
