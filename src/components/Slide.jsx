0f4ece5d79ad07a3ee649045d48fde1370d18b40
Juan José Mayorquín <juanjmayor@outlook.es>
2023-09-20 16:25:55 -0500

    Se logra la animación de hover en Slides.jsx

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,25 +1,23 @@
-export function Slide() {
+export function Slide({ year, text, image, color}) {
+  const bgColor = {
+    backgroundImage: `url(${image})`,
+    backgroundSize: "cover",
+    backgroundColor: color,
+  }
   return (
     <div className="h-screen max-h-screen relative animate-fade animate-once animate-duration-1000 animate-delay-300 flex items-center">
-      <div className="grid grid-cols-2 relative w-[95%] m-auto">
-        <div className="bg-red-700 fixed h-[130vh] w-1/2 -left-28 -top-32 -z-50 rotate-[8deg] hover:w-7/12 transition-all"></div>
-        <div className="">
-          <h2 className="font-bold text-[14rem] col-span-1 text-white">1970</h2>
+      <div className="grid grid-cols-4 h-full w-full relative">
+        <div className=" col-start-1 w-11/12 hover:w-full col-span-2 h-full relative transition-all">
+          <div style={bgColor} className="h-full w-11/12 flex flex-col justify-center items-center">
+            <h2 className="text-[12rem] font-bold text-white">{year}</h2>
+            <div style={bgColor} className="absolute min-h-full w-2/12 top-0 right-3 -skew-x-6"></div>
+          </div>
         </div>
-        <p className="text-xl text-justify">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-          veritatis rerum? Pariatur error distinctio repudiandae dicta quidem
-          maxime dolorem, ullam eos sint explicabo exercitationem! Deserunt
-          delectus est non perspiciatis ratione omnis, fugit accusamus corrupti
-          eveniet tempore consequuntur! Natus incidunt placeat doloremque,
-          magnam perferendis voluptas dignissimos quaerat aut saepe eligendi!
-          Dolorem distinctio soluta illo laborum explicabo aliquam adipisci id
-          rem amet maxime. Dolor porro perspiciatis deleniti sequi excepturi
-          doloremque eligendi, nostrum velit vero, earum ratione vel!
-          Repudiandae error, quasi, ad laboriosam veniam illo amet commodi quae
-          nihil, ipsam accusantium culpa neque quo necessitatibus porro labore?
-          Labore minus illo inventore consectetur expedita?
-        </p>
+        <div className="col-start-3 col-span-4 h-full flex flex-col justify-center items-center bg-white">
+          <p className="leading-8 text-xl text-justify w-11/12">
+            {text}
+          </p>
+        </div>
       </div>
     </div>
   );
