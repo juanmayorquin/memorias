8b191c13e8e55e4b424b448f12cff13ffcb83d94
Juan José Mayorquín <juanjmayor@outlook.es>
2023-10-06 07:40:48 -0500

    Se añade animación en el Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,7 +4,7 @@ import { profileContext } from "../context/profile-context";
 export function Modal({video}) {
     const { updateModal } = useContext(profileContext);
     return (
-        <div className="absolute z-50 bg-black/80 h-full w-full flex flex-col justify-center items-center" onClick={updateModal}>
+        <div className="animate-fade absolute z-50 bg-black/80 h-full w-full flex flex-col justify-center items-center" onClick={updateModal}>
             <div className="bg-white px-0 py-8 w-1/2 rounded-lg flex flex-col">
                 <video id="video" className="w-11/12 mx-auto" src={video} controls></video>
             </div>
